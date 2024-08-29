26bd746f350650529402bac61be127c942c09b94
Penghua Chen <chen756415756@outlook.com>
2024-08-30 01:08:37 +0800

    Finished PMD901 driver

--- pmd901_driver.sv
+++ pmd901_driver.sv
@@ -24,3 +24,24 @@ endclass: pmd901_driver
 function pmd901_driver::new(string name = "pmd901_driver", uvm_component parent = null);
   super.new(name, parent);
 endfunction
+
+function pmd901_driver::build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    m_cfg = pmd901_agent_config::get_config(this);
+    m_bfm = m_cfg.drv_bfm;
+endfunction: build_phase
+
+task pmd901_driver::run_phase(uvm_phase phase);
+    pmd901_trans req;
+    pmd901_trans rsp;
+
+    m_bfm.wait_inputs_isknown();
+    forever begin
+        seq_item_port.get_next_item(req);
+        m_bfm.setup_phase(req);
+        seq_item_port.item_done(); 
+        seq_item_port.get_next_item(rsp);
+        m_bfm.access_phase(rsp);
+        seq_item_port.item_done();
+    end
+endtask
