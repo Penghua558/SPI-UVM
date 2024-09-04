d2b331d64475c034e0601a5a5e5762161421a755
Penghua Chen <chen756415756@outlook.com>
2024-09-05 00:15:01 +0800

    Added agent folder and moved APB agent and PMD901 slave agent to this folder

---
+++ agents/pmd901_slave_agent/pmd901_agent_pkg.sv
@@ -0,0 +1,13 @@
+package pmd901_agent_pkg;
+
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+`include "pmd901_trans.sv"
+`include "pmd901_agent_config.sv"
+
+`include "pmd901_driver.sv"
+`include "pmd901_sequencer.sv"
+`include "pmd901_sequence.sv"
+
+`include "pmd901_agent.sv"
+endpackage: spi_agent_pkg
--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ export LM_LICENSE_FILE=27020@10.116.0.5
 export VCS_HOME=/apps/vcsmx/vcs/U-2023.03-SP2/
 export HOME=/home/runner
 export UVM_HOME=/apps/vcsmx/vcs/U-2023.03-SP2//etc/uvm-1.2
-vcs -full64 -licqueue '-timescale=1ns/10ps' '+vcs+flush+all' '+warn=all' '-sverilog' +incdir+$UVM_HOME/src $UVM_HOME/src/uvm.sv $UVM_HOME/src/dpi/uvm_dpi.cc -CFLAGS -DVCS design.sv testbench.sv  && ./simv +vcs+lic+wait '+UVM_TESTNAME=spi_interrupt_test'  ; echo 'Creating result.zip...' && zip -r /tmp/tmp_zip_file_123play.zip . && mv /tmp/tmp_zip_file_123play.zip result.zip
+vcs -full64 -licqueue '-timescale=1ns/10ps' '+vcs+flush+all' '+warn=all' '-sverilog' +incdir+$UVM_HOME/src $UVM_HOME/src/uvm.sv $UVM_HOME/src/dpi/uvm_dpi.cc -CFLAGS -DVCS RTL/design.sv testbench.sv  && ./simv +vcs+lic+wait '+UVM_TESTNAME=spi_interrupt_test'  ; echo 'Creating result.zip...' && zip -r /tmp/tmp_zip_file_123play.zip . && mv /tmp/tmp_zip_file_123play.zip result.zip
