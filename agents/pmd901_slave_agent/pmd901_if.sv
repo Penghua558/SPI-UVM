d37f5e24510f3104d95448e3be7a9b060a65c8c1
Penghua Chen <chen756415756@outlook.com>
2024-09-14 15:03:50 +0800

    Changed permission of pmd901_sequence and pmd901_bus_sequence to rw, moved assertion from pmd901_monitor_bfm to pmd901_if

--- agents/pmd901_slave_agent/pmd901_if.sv
+++ agents/pmd901_slave_agent/pmd901_if.sv
@@ -8,3 +8,43 @@ interface pmd901_if;
   logic ready;
   logic park;
 endinterface: pmd901_if
+
+event spi_clk_event;
+bit seq_start;
+initial begin
+forever begin
+    fork
+        begin
+            @(negedge cs_n);
+            seq_start = 1'b1;
+        end
+        begin
+            @(posedge cs_n);
+            seq_start = 1'b0;
+        end
+        begin
+            @(posedge clk);
+            seq_start = 1'b0;
+        end
+    join_any
+    disable fork;
+    -> spi_clk_event;
+end
+end
+
+property clocknumber;
+    @(spi_clk_event) seq_start |=> !csn[*16] ##1 !$rose(clk);
+endproperty
+
+property readyfan;
+    @(ready, fan) !(ready & fan);
+endproperty
+
+
+assert property (clocknumber)
+else
+    `uvm_fatal("PMD901 MONITOR BFM", "Clock cycle number is not 16 during data transmission!")
+
+assert property (readyfan)
+else
+    `uvm_fatal("PMD901 MONITOR BFM", "Ready and fan should not be asserted at the same time")
--- agents/pmd901_slave_agent/pmd901_monitor_bfm.sv
+++ agents/pmd901_slave_agent/pmd901_monitor_bfm.sv
@@ -14,10 +14,7 @@ clocking mon_cb@(posedge clk);
    input mosi; 
 endclocking: mon_cb 
 
-`include "uvm_macros.svh"
-import uvm_pkg::*;
 import pmd901_agent_pkg::*;
-import pmd901_agent_dec::*;
 
 //------------------------------------------
 // Data Members
@@ -92,67 +89,5 @@ task run();
         proxy.notify_transaction(cloned_item);
     end
 endtask
-
-//------------------------------------------
-// Assertions 
-//------------------------------------------
-property csnhold;
- realtime current_time;
-  ($fell(csn),current_time=$realtime) |=>
- first_match(##[1:$]($fell(clk) or $rose(clk))) ##0 (($realtime - current_time) >= 0.4us);
-endproperty
-
-property csnsetup;
- realtime current_time;
-  ($rose(csn),current_time=$realtime) |=>
- first_match(##[1:$]($fell(csn))) ##0 (($realtime - current_time) >= 20us);
-endproperty
-
-event spi_clk_event;
-bit seq_start;
-initial begin
-forever begin
-    fork
-        begin
-            @(negedge cs_n);
-            seq_start = 1'b1;
-        end
-        begin
-            @(posedge cs_n);
-            seq_start = 1'b0;
-        end
-        begin
-            @(posedge clk);
-            seq_start = 1'b0;
-        end
-    join_any
-    disable fork;
-    -> spi_clk_event;
-end
-end
-
-property clocknumber;
-    @(spi_clk_event) seq_start |=> !csn[*16] ##1 !$rose(clk);
-endproperty
-
-property readyfan;
-    @(ready, fan) !(ready & fan);
-endproperty
-
-assert property (csnhold)
-else
-    `uvm_fatal("PMD901 MONITOR BFM", "CS_N hold time check failed!")
-
-assert property (csnsetup)
-else
-    `uvm_fatal("PMD901 MONITOR BFM", "CS_N setup time check failed!")
-
-assert property (clocknumber)
-else
-    `uvm_fatal("PMD901 MONITOR BFM", "Clock cycle number is not 16 during data transmission!")
-
-assert property (readyfan)
-else
-    `uvm_fatal("PMD901 MONITOR BFM", "Ready and fan should not be asserted at the same time")
 
 endinterface: pmd901_monitor_bfm
